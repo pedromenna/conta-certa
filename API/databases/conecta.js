de280aeb5bea34bd4427d3ba41ff5400a3f451e2
Gabriela Cavalheiro <gabicavalheirof@gmail.com>
2023-10-20 14:50:05 -0300

    api

--- API/databases/conecta.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-export const sequelize = new Sequelize(
-  "railway", "root", "vyr2OWhLj3PxJ4du9Emr", {
-  dialect: "mysql",
-  host: "containers-us-west-197.railway.app",
-  port: 5876
-});
