de280e72b4da263006eaac722168993c6ace0be4
dode2511 <leondodejr@gmail.com>
2023-10-20 19:24:19 -0300

    :raising_hand:  separacao de arquivos

--- api/Api-Conta-Certa-main/databases/conecta.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
-
-export const sequelize = new Sequelize(
-  DB_NAME, DB_USER, DB_PASSWORD, {
-  dialect: "mysql",
-  host: DB_HOST,
-  port: DB_PORT
-});
